3d452a7ba7849875216fe0a04b0730d7744e43b6
Skux4life <mattellis1234@gmail.com>
2025-02-03 13:52:53 +1100

    start the contact us component

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,20 @@
+import ContactForm from "./ContactForm";
+
+const ContactUs = () => {
+  return (
+    <div className="bg-blue-600 grid grid-cols-2 m-48 py-24 px-12 gap-24">
+      <div className="">
+        <h1 className="text-4xl text-white font-mono mb-8">Contact Us</h1>
+        <p className="text-gray-200 leading-loose font-thin">
+          Get in touch with us! Whether you have questions, feedback, or just
+          want to say hello, we're here for you.
+        </p>
+      </div>
+      <div>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
